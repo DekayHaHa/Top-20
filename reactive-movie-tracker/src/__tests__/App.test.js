2063f3125ef900e25655851ebdda902813af7df1
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-03-19 23:18:48 -0600

    Fixed the app test

--- reactive-movie-tracker/src/__tests__/App.test.js
+++ reactive-movie-tracker/src/__tests__/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
 import App from '../containers/App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
+describe("App", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App/>);
+  });
+
+
+it("should match the snapshot with all data passed in correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
